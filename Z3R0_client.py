911e0e381f13b43f3a05730865898e18178495b2
Shadow_ <95306026+OnixZeny@users.noreply.github.com>
2022-04-14 09:08:10 -0500

    Add files via upload
    el Z3R0_client.py 
    es el cliente con el que el servidor python se comunican para poder crear un archivo el cual comprometa a la victima con el atacante de manera remota y se puedan ejecutar comandos.

---
+++ Z3R0_client.py
@@ -0,0 +1,28 @@
+import socket
+import subprocess
+
+mi_socket = socket.socket()
+mi_socket = socket.bind( ('url', 80) )
+mi_socket = socket.listen(20)
+
+while True:
+
+#variables
+ subprocess.call("dir", shell=True) 
+ subprocess.call("cd", shell=True)
+ subprocess.call("cls", shell=True)
+
+
+#funcion
+
+def createFile():
+    data_post = {
+        ''}
+if __name__ == '__main__':
+    createFile()
+
+
+
+    mi_socket, addr = mi_socket.accept()
+    mi_socket.send()
+mi_socket.close()
